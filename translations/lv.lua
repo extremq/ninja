32e00e0dfa2b991b1e72d2c1e9c90fac714d3513
syriusnoir <61652183+syriusnoir@users.noreply.github.com>
2020-06-09 19:04:28 +0300

    Create lv.lua

---
+++ translations/lv.lua
@@ -0,0 +1,39 @@
+translations.lv = {
+    lastTime = "Tavs pēdējais laiks",
+    lastBestTime = "Tavs labākais laiks",
+    helpToolTip = "<p align='center'>Spied <b>H</b>, lai atvērtu palīdzību.</p>",
+    optionsYes = "<font color='#53ba58'>Jā</font>",
+    optionsNo = "<font color='#ba5353'>Nē</font>",
+    graffitiSetting = "Iespējot grafiti",
+    particlesSetting = "Iespējot paātrināšanas/lēkšanas efektus",
+    timePanelsSetting = "Iespējot laika paneļus",
+    globalChatSetting = "Iespējot globālo čatu",
+    voteStart = " sāka balsojumu esošās mapes izlaišanai. Raksti !yes, lai balsotu par.",
+    newRecord = " pabeidza mapi visīsākajā laikā!",
+    devInfo = "<V>Vēlies iesniegt mapi? Skaties šeit: https://atelier801.com/topic?f=6&t=888399</V>\n<font color='#CB546B'>Šis modulis ir izveides procesā. Lūdzu, ziņo par jebkādām kļūdām Extremq#0000 vai Railysse#0000.</font>",
+    discordInfo = "<BV>Pievienojies mūsu Discord! https://discord.gg/WawZVaq</BV>",
+    welcomeInfo = "Esi sveicināts <font color='#E68D43'>#ninja</font>! Spied <font color='#E68D43'>H</font>, lai atvērtu palīdzību.",
+    finishedInfo = "Tu pabeidzi mapi! Laiks: ",
+    helpBody = "Tev ir jānogādā siers atpakaļ uz alu, cik vien ātri spēj.\n\n<b>Spējas</b>:\n» Paātrināšana - Nospied <b><font color='#CB546B'>kreiso</font></b> vai <b><font color='#CB546B'>labo bultiņu</font></b> divreiz. (1s noildze)\n» Lēkšana - Nospied <b><font color='#CB546B'>augšējo bultiņu</font></b> divreiz. (3s noildze)\n» Attīt - Nospied <b><font color='#CB546B'>atstarpi</font></b>, lai izveidotu atskaites punktu. Nospied <b><font color='#CB546B'>atstarpi</font></b> vēlreiz 3 sekunžu laikā, lai teleportētos atpakaļ uz to. (10s noildze)\n\n<b>Citi īsceļi</b>:\n» Peles nogalināšana - Nospied <b><font color='#CB546B'>X</font></b> vai ieraksti /mort, lai nogalinātu peli.\n» Atvērt izvēlni - Nospied <b><font color='#CB546B'>M</font></b> vai klikšķini ekrāna kreisajā pusē, lai  atvērtu/aizvērtu izvēlni.\n» Atstāt grafiti - Nospied <b><font color='#CB546B'>C</font></b>, lai atstātu grafiti. (60s noildze)\n» Atvērt palīdzību - Nospied <b><font color='#CB546B'>H</font></b>, lai atvērtu/aizvērtu šo logu.\n\n<b>Komandas</b>:\n» !p Vārds#id - Pārbauda cita spēlētāja statistiku.\n» !pw Parole - Iestata paroli istabai. (istabai ir jābūt tevis veidotai)\n» !m @kods - Ielādē jebkuru mapi, kuru vēlies. (istabai ir jābūt parolei)\n» !langue valsts - Maina moduļa valodu. (tikai tev)\n\n<p align='center'><a href='event:CloseMenu'><b><font color='#CB546B'>Aizvērt</font></b></a></p>", --23
+    Xbtn = "X",
+    -- IF YOUR LANGUAGE HAS SEPCIAL CHARACTERS, PLEASE JUST LEAVE THESE AS DEFAULT
+    shopTitle = "Kolekcija",
+    profileTitle = "Profils",
+    leaderboardsTitle = "Tablo",
+    settingsTitle = "Opcijas",
+    aboutTitle = "Par",
+    -- END
+    aboutBody = "Moduli kodēja <font color='#FFD991'>Extremq#0000</font>.\nIdejas spēlei, kļūdu testēšanu un mapes nodrošināja <font color='#FFD991'>Railysse#0000</font>.\n\nŠo moduli pilnībā atbalsta peļu organizācija „Red Cheese” ar „Save Module” projektu. Visas iegūtās finanses tiks ziedotas pelēm, kuras spēlē #parkour, lai varam piekukuļot viņas spēlēt mūsu moduli.\n\nMēs tikai jokojam, paldies, ka izmēģinājāt mūsu moduli! :D\n\n<p align='center'><font color='#EB1D51'>&lt;3</font></p>", -- 30
+    playtime = "Playtime",
+    firsts = "Pirmās vietas",
+    finishedMaps = "Pabeigtās mapes",
+    firstRate = "Pirmo vietu attiecība",
+    holeEnters = "Reizes, cik ieiets alā",
+    graffitiUses = "Reizes, cik lietots grafiti",
+    dashUses = "Reizes, cik lietota paātrināšana",
+    rewindUses = "Reizes, cik lietota attīšana",
+    hardcoreMaps = "Pabeigtās „Hardcore” mapes",
+    shopNotice = "Veikals ir izveides procesā.",
+    leaderboardsNotice = "Rezultātu tablo tiks ieviests, kad modulis kļūs oficiāls.",
+    notValidCommand = "nav derīga komanda."
+}
