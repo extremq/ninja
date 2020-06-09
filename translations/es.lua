ee86b0acfd1b5df625802be638f1da575df88c37
Iván <tocutoeltuco@gmail.com>
2020-06-09 16:22:06 -0300

    Create es.lua

---
+++ translations/es.lua
@@ -0,0 +1,39 @@
+translations.es = {
+    lastTime = "Tu último tiempo",
+    lastBestTime = "Tu record",
+    helpToolTip = "<p align='center'>Presiona <b>H</b> para obtener ayuda.</p>",
+    optionsYes = "<font color='#53ba58'>Sí</font>",
+    optionsNo = "<font color='#ba5353'>No</font>",
+    graffitiSetting = "Activar graffitis",
+    particlesSetting = "Activar partículas de impulso/salto",
+    timePanelsSetting = "Activar paneles de tiempos",
+    globalChatSetting = "Activar chat global",
+    voteStart = " empezó a votar para saltar el mapa. Escribe !yes para votar.",
+    newRecord = " finalizó el mapa con el tiempo más rápido!",
+    devInfo = "<V>¿Quieres enviar un mapa? Usa este enlace: https://atelier801.com/topic?f=6&t=888399</V>\n<font color='#CB546B'>El módulo está en desarrollo. Por favor reporta cualquier bug a Extremq#0000 o Railysse#0000.</font>",
+    discordInfo = "<BV>¡Únete a nuestro discord! https://discord.gg/WawZVaq</BV>",
+    welcomeInfo = "¡Bienvenido a <font color='#E68D43'>#ninja</font>! Presiona <font color='#E68D43'>H</font> para obtener ayuda.",
+    finishedInfo = "¡Finalizaste el mapa! Tiempo: ",
+    helpBody = "Tienes que traer el queso de vuelta al agujero tan rápido como puedas.\n\n<b>Habilidades</b>:\n» Impulso - Presiona <b><font color='#CB546B'>Izquierda</font></b> o <b><font color='#CB546B'>Derecha</font></b> dos veces. (espera de 1s)\n» Salto - Presiona <b><font color='#CB546B'>Arriba</font></b> dos veces. (espera de 3s)\n» Rebobinar - Presiona <b><font color='#CB546B'>Espacio</font></b> para dejar un checkpoint. Presiona <b><font color='#CB546B'>Espacio</font></b> de nuevo dentro de 3 segundos para teletransportarte al checkpoint. (espera de 10s)\n\n<b>Otros atajos</b>:\n» Matar tu ratón - Presiona <b><font color='#CB546B'>X</font></b> o escribe /mort para matar a tu ratón.\n» Abrir menú - Presionar <b><font color='#CB546B'>M</font></b> o clickea en el lado izquierdo de tu pantalla para abrir/cerrar el menú.\n» Colocar un graffiti - Presiona <b><font color='#CB546B'>C</font></b> para dejar un graffiti. (espera de 60s)\n» Abrir ayuda - Presiona <b><font color='#CB546B'>H</font></b> para abrir/cerrar esta ventana.\n\n<b>Comandos</b>:\n» !p Nombre#id - Ver las estadísticas de otro jugador.\n» !pw Contraseña - Cambiar la contraseña de la sala. (debe ser creada por tí)\n» !m @code - Cargar cualquier mapa que quieras. (la sala debe tener una contraseña)\n» !langue país - Cambiar el lenguaje del módulo. (sólo para tí)\n\n<p align='center'><a href='event:CloseMenu'><b><font color='#CB546B'>Cerrar</font></b></a></p>", --23
+    Xbtn = "X",
+    -- IF YOUR LANGUAGE HAS SEPCIAL CHARACTERS, PLEASE JUST LEAVE THESE AS DEFAULT
+    shopTitle = "Tienda",
+    profileTitle = "Perfil",
+    leaderboardsTitle = "Rankings",
+    settingsTitle = "Configuracion",
+    aboutTitle = "Acerca",
+    -- END
+    aboutBody = "Módulo programado por <font color='#FFD991'>Extremq#0000</font>.\nIdeas de juego, testeo de errores y mapas por <font color='#FFD991'>Railysse#0000</font>.\n\nEste módulo está totalmente respaldado por la fundación de ratones „Red Cheese” con el proyecto „Save Module”. Todas las donaciones que ganemos serán donadas a los ratones que juegan #parkour para sobornarlos y que vengan a jugar nuestro módulo.\n\nSólo estamos bromeando, ¡gracias por jugar nuestro módulo! :D\n\n<p align='center'><font color='#EB1D51'>&lt;3</font></p>", -- 30
+    playtime = "Tiempo de juego",
+    firsts = "Firsts",
+    finishedMaps = "Mapas completados",
+    firstRate = "Porcentaje de firsts",
+    holeEnters = "Veces que entró al agujero",
+    graffitiUses = "Usos del graffiti",
+    dashUses = "Veces que usó un impulso",
+    rewindUses = "Veces que rebobinó",
+    hardcoreMaps = "Mapas difíciles completados",
+    shopNotice = "La tienda está en desarrollo.",
+    leaderboardsNotice = "Una tabla de clasificación será implementada cuando el módulo se vuelva oficial.",
+    notValidCommand = "no es un comando válido."
+}
