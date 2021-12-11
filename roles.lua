497abee30cff7990c6bd94ec4968ee10592a014d
extremq <stefansteff999@gmail.com>
2021-12-11 16:09:09 +0200

    Update Extremq#0000 -> Extremq#0020. Everyone say 'Congratulations!'.

--- roles.lua
+++ roles.lua
@@ -2,9 +2,9 @@
     name: roles.lua
     description: contains roles (mapper, mod, translator, etc.)
 ]]--
-modList = {['Extremq#0000'] = true, ['Railysse#0010'] = true, ['Syrius#8114'] = true}
+modList = {['Extremq#0020'] = true, ['Railysse#0010'] = true, ['Syrius#8114'] = true}
 translatorList = {['Extremq#0000'] = true, ['Railysse#00100'] = true, ['Syrius#8114'] = true, ['Jaker#9310'] = true, ['Tocutoeltuco#0000'] = true, ['Osmanyksk123#5925'] = true, ['Poklava#0000'] = true}
-devList = {['Extremq#0000'] = true, ['Railysse#0010'] = true, ['Syrius#8114'] = true}
+devList = {['Extremq#0020'] = true, ['Railysse#0010'] = true, ['Syrius#8114'] = true}
 mapperList = {['Railysse#0010'] = true}
 modRoom = {}
 opList = {}
